ffad549b3b5364b4ca9553e31d19b5776d2927e1
Hector <hmillerbakewell@gmail.com>
2017-03-05 16:25:49 +0000

    Removing superfluous files and folders
    Removing the build folder
    Removing the tests file

--- build/RamerDouglasPeucker.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function perpendicularDistance(pOffset: number[], pStart: number[], pEnd: number[]): number;
-/**
- * Returns all but the last waypoint as described by Ramer-Douglas-Peucker
- * @param pointList
- * @param epsilon
- */
-export declare function RamerDouglasPeucker(pointList: number[][], epsilon: number): number[][];
--- build/freehand-zx-json-io.d.ts
+++
@@ -1,8 +0,0 @@
-import Diagrams = require("./freehand-diagrams.js");
-import DiagramIO = require("./freehand-io.js");
-export declare class ZXJSONIOModule extends DiagramIO.DiagramIOHTMLModule {
-    targetDiagram: Diagrams.Diagram;
-    constructor(targetDiagram: Diagrams.Diagram);
-    onDiagramChange(): void;
-    toSimpleZXGraph(): string;
-}
